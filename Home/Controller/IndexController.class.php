fde53ec8bb92c60e54ec49f75c7d70ab07d7d326
Jay Lin <weijingjaylin@gmail.com>
2016-03-23 17:12:02 -0700

    update db

--- Home/Controller/IndexController.class.php
+++ Home/Controller/IndexController.class.php
@@ -3,6 +3,8 @@ namespace Home\Controller;
 use Think\Controller;
 class IndexController extends Controller {
     public function index(){
+        $model_test = D('test_under');
+        $model_test->sayHi();
         $model_test = D('test_under', 'Common');
         $model_test->sayHi();
     }
--- index.php
+++ index.php
@@ -1,3 +1,3 @@
 <?php 
   define('APP_DEBUG', true);
-  require_once '..\..\framework\thinkphp\3.2.3\ThinkPHP.php';
+  require_once '..\..\..\framework\thinkphp\3.2.3\ThinkPHP.php';
