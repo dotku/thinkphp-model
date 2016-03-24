6c5af3988428a63b94e7040721abb5ffdaf67401
Jay Lin <weijingjaylin@gmail.com>
2016-03-23 17:14:52 -0700

    remove home model

--- Home/Controller/IndexController.class.php
+++ Home/Controller/IndexController.class.php
@@ -3,9 +3,9 @@ namespace Home\Controller;
 use Think\Controller;
 class IndexController extends Controller {
     public function index(){
+        //$model_test = D('test_under');
+        //$model_test->sayHi();
         $model_test = D('test_under');
-        $model_test->sayHi();
-        $model_test = D('test_under', 'Common');
         $model_test->sayHi();
     }
 }
